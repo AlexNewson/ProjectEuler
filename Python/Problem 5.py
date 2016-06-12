5f9b75a166f9fef3dab20522b2665b65e7507294
Alex Newson <git@alexnewson.co.uk>
2016-06-12 16:10:12 +0100

    Solution 5

--- Python/Problem 5.py
+++ Python/Problem 5.py
@@ -1,13 +1,24 @@
 print('''
-Problem No. 4
+Problem No. 5
 Title:      Smallest multiple
 
 Problem:    2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
             What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 ''')
-fail = True
+notFound = True
 number = 0
+numberList = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+score = 0
 
-while fail:
-    number += 1
-    
+while notFound:
+    score = 0
+    number += 2520
+
+    for x in numberList:
+        if number % x == 0:
+            score += 1
+
+    if score == 10:
+        break
+
+print(number)
