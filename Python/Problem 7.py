2b64d9f315247d4e17f311ae49ef392a9d3be5e7
Alex Newson <git@alexnewson.co.uk>
2016-06-14 18:33:12 +0100

    Solution 7

--- Python/Problem 7.py
+++ Python/Problem 7.py
@@ -5,3 +5,27 @@ Title:      10001st prime
 Problem:    By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
             What is the 10 001st prime number?
 ''')
+
+def isPrime(x):
+    if x % 2 == 0:
+        return False
+
+    p = 3
+    while p < x**0.5+1:
+        if x % p == 0: return False
+        p += 2
+    return True
+
+def nthPrime(n):
+    prime = 2
+    primes = 1
+    number = 3
+    while primes < n:
+        if isPrime(number):
+            prime = number
+            primes += 1
+        number += 1
+    return prime
+
+answer = nthPrime(10001)
+print(answer)
