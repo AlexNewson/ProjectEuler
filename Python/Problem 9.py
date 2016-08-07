bbba0d7df3495805b380df511cca8c721ec5d19d
Alex Newson <git@alexnewson.co.uk>
2016-08-07 11:56:18 -0600

    Slow Solution

--- Python/Problem 9.py
+++ Python/Problem 9.py
@@ -4,9 +4,33 @@ Title:      Special Pythagorean triplet
 
 Problem:    A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
-            a2 + b2 = c2
-            For example, 32 + 42 = 9 + 16 = 25 = 52.
+            a^2 + b^2 = c^2
+            For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 
             There exists exactly one Pythagorean triplet for which a + b + c = 1000.
             Find the product abc.
 ''')
+
+# A = 200
+# B = 375
+# C = 425
+
+a = 0
+b = 0
+c = 0
+
+while a < 998:
+    b = 0
+    a += 1
+    while b < 999:
+        c = 0
+        b += 1
+        while c < 1000:
+            c += 1
+
+            if a + b + c == 1000:
+                if a**2 + b**2 == c**2:
+                    print a
+                    print b
+                    print c
+                    break
