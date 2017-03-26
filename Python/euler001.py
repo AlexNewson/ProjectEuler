afddd7b6db5982833d70da134f67f97e9cfc5e2a
Alex Newson <git@alexnewson.co.uk>
2017-03-26 17:08:42 +0100

    formatting

--- Python/euler001.py
+++ Python/euler001.py
@@ -6,6 +6,7 @@ Problem:    If we list all the natural numbers below 10 that are multiples of 3 
             The sum of these multiples is 23.
             Find the sum of all the multiples of 3 or 5 below 1000.
 ''')
+
 import time
 
 start = time.time()
