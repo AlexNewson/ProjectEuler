7548d1e0f12e8d35c90b35039da6ecfcda8db4b1
Alex Newson <git@alexnewson.co.uk>
2017-03-31 12:14:07 +0100

    Updated Templaye to .py file

---
+++ Python/euler000.py
@@ -0,0 +1,24 @@
+print('''
+Problem No. 00
+Title:      Template
+
+Problem:    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Sed pharetra augue ut urna mollis lacinia. Mauris vitae nunc ac est
+            molestie ultricies quis a elit. Praesent semper suscipit molestie.
+            Mauris ac justo id sem facilisis auctor nec non lacus. Cras maximus
+            et velit eu ornare. In imperdiet venenatis dui. Sed ac metus vel
+            libero aliquet semper.
+''')
+
+import time
+
+start = time.time()
+
+# ==================================================
+# Put Code here
+# ==================================================
+
+end = time.time()
+time = end - start
+
+print("This took %s seconds" % time)
