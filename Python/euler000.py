a111909391689adbe5396ea011886112d9a16b19
Alex Newson <git@alexnewson.co.uk>
2022-04-11 20:23:34 +0100

    Template now use function to grab metadata from online

--- Python/euler000.py
+++ Python/euler000.py
@@ -1,17 +1,7 @@
-print('''
-Problem No. 00
-Title:      Template
-
-Problem:    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Sed pharetra augue ut urna mollis lacinia. Mauris vitae nunc ac est
-            molestie ultricies quis a elit. Praesent semper suscipit molestie.
-            Mauris ac justo id sem facilisis auctor nec non lacus. Cras maximus
-            et velit eu ornare. In imperdiet venenatis dui. Sed ac metus vel
-            libero aliquet semper.
-''')
-
 import time
+from euler import print_problem
 
+print_problem(1)
 start = time.time()
 
 # ==================================================
