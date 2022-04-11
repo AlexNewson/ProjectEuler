cb0a25f101a804447b0c65321c74a889793f1591
Alex Newson <git@alexnewson.co.uk>
2022-04-11 21:26:16 +0100

    Problem 44

--- Python/euler044.py
+++ Python/euler044.py
@@ -1,21 +1,38 @@
-print('''
-Problem No. 00
-Title:      Template
-
-Problem:    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Sed pharetra augue ut urna mollis lacinia. Mauris vitae nunc ac est
-            molestie ultricies quis a elit. Praesent semper suscipit molestie.
-            Mauris ac justo id sem facilisis auctor nec non lacus. Cras maximus
-            et velit eu ornare. In imperdiet venenatis dui. Sed ac metus vel
-            libero aliquet semper.
-''')
-
+import sys
 import time
+from euler import print_problem
+from math import sqrt
+from itertools import combinations
 
+print_problem(44)
 start = time.time()
 
 # ==================================================
-# Put Code here
+
+
+def gen_pen(n):
+    return int((n * (3 * n - 1)) / 2)
+
+
+def is_pen(x):
+    return ((1+sqrt(1+24*x))/6).is_integer()
+
+
+pentagonal_numbers = []
+lowest_d = sys.maxsize
+
+for i in range(1, 2500):
+    pentagonal_numbers.append(gen_pen(i))
+
+for pair in combinations(pentagonal_numbers, 2):
+    s = pair[0] + pair[1]
+    if is_pen(s):
+        d = abs(pair[0] - pair[1])
+        if is_pen(d):
+            if d < lowest_d:
+                lowest_d = d
+
+print("The Answer is: %i" % lowest_d)
 # ==================================================
 
 end = time.time()
