ec0ce4eb4eaf73bba71d6edeb179afaa2039af8d
alexnewson <git@alexnewson.co.uk>
2022-04-13 19:44:26 +0000

    Added comments for Problem 46

--- Python/euler046.py
+++ Python/euler046.py
@@ -28,19 +28,25 @@ number = 9
 answer = 0
 
 while answer == 0:
-    # Incriment
+    # Increment number by one
     number += 1
 
+    # Check if number is odd
     if number % 2 == 1:
+        # Check if it's a composite odd, (i.e. not a prime)
         if not is_prime(number):
             x = 0
             while True:
+                # Increment the "twice a square number"
                 x += 1
+                # Calculate the potential prime number needed to make the formula work
                 prime = number - (2 * (x ** 2))
+                # If it's a prime, we've found a number that fits the conjecture
+                if is_prime(prime):
+                    break
+                # If we reach 0 and still haven't found a prime, the number breaks the conjecture
                 if prime < 0:
                     answer = number
-                    break
-                if is_prime(prime):
                     break
 
 print("The Answer is: %i" % answer)
