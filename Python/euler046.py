70f3b44e4b79f551997190ea14844a74bb11125b
Alex Newson <git@alexnewson.co.uk>
2022-04-13 15:08:29 +0100

    Progress on Problem 46

---
+++ Python/euler046.py
@@ -0,0 +1,53 @@
+import time
+from euler import print_problem
+from math import sqrt
+
+print_problem(46)
+start = time.time()
+
+# ==================================================
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    r = int(sqrt(n))
+    f = 5
+    while f <= r:
+        if n % f == 0 or n % (f + 2) == 0:
+            return False
+        f += 6
+    return True
+
+
+number = 9
+answer = 0
+
+while answer == 0:
+    number += 1
+
+    if number % 2 == 1:
+        if not is_prime(number):
+            x = 0
+            while True:
+                x += 1
+                prime = number - (2 * (x ^ 2))
+                if prime > number:
+                    answer = number
+                    break
+                if is_prime(prime):
+                    break
+                else:
+                    x += 1
+
+print("The Answer is: %i" % answer)
+# ==================================================
+
+end = time.time()
+time = end - start
+
+print("This took %s seconds" % time)
